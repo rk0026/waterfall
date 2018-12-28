3f8ed5aecf5df704a18b32d260ba9bb86493a482
Jimmy <abc751202@gmail.com>
2018-12-28 12:04:30 +0800

    Updates

---
+++ precache-manifest.a8fb061153f286d709cd83dfe651208a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "208aa44e508fb587e63b",
+    "url": "/waterfall/static/js/runtime~main.208aa44e.js"
+  },
+  {
+    "revision": "b27e651f169305c9d7a2",
+    "url": "/waterfall/static/js/main.b27e651f.chunk.js"
+  },
+  {
+    "revision": "3179df03de39302d4088",
+    "url": "/waterfall/static/js/1.3179df03.chunk.js"
+  },
+  {
+    "revision": "b27e651f169305c9d7a2",
+    "url": "/waterfall/static/css/main.ee1160e8.chunk.css"
+  },
+  {
+    "revision": "88e839cf578d33c260c047cc035546b7",
+    "url": "/waterfall/index.html"
+  }
+];
